fb3d1e91803992662419e659e5c738b5707545f5
rliebz <rliebz@gmail.com>
2014-03-17 13:18:29 -0400

    Player class refactoring and improved logic.

--- Objects/Player.cs
+++ Objects/Player.cs
@@ -62,30 +62,108 @@ namespace Puddle
             shot_point = 0;
             jump_point = 0;
 
-            // Sprite Business
+            // Sprite Information
             frameIndex = 0;
         }
 
-        // TODO: Add all freezing effects here.
-        // Since a number of effects may cause the character to be
-        // unable to move, a single property can check for all of them.
+        // Property determining if the character can act
         public bool frozen()
         {
             return (puddled);
         }
 
+        // Property determining if the character can be hurt
+        public bool invulnerable()
+        {
+            return (puddled && frameIndex == 5 * 32);
+        }
+
         public void Update(Controls controls, Physics physics, 
             ContentManager content)
         {
+            // Move based on controls and physics
             Move(controls, physics);
 
-            // Puddle
+            // Puddle based on controls
             if (controls.isPressed(Keys.Down, Buttons.DPadDown) &&
                 !frozen() && grounded && powerup["puddle"])
             {
                 puddled = true;
             }
 
+            // Shoot based on controls
+            Shoot(controls, physics, content);
+
+            // Jump based on controls
+            Jump(controls, physics);
+
+            // Check for collisions
+            CheckCollisions(physics);
+
+            // Animate sprite
+            Animate(controls, physics);
+        }
+
+
+        public void CheckCollisions(Physics physics)
+        {
+            // Check enemy collisions
+            if (!invulnerable())
+            {
+                foreach (Enemy e in physics.enemies)
+                {
+                    if (Math.Sqrt(Math.Pow(spriteX - e.spriteX, 2) +
+                        Math.Pow(spriteY - e.spriteY, 2)) < 32)
+                    {
+                        spriteX = 400;
+                        spriteY = -32;
+                        y_vel = 0;
+                        grounded = false;
+                        puddled = false;
+                    }
+                }
+            }
+        }
+
+        private void Move(Controls controls, Physics physics)
+        {
+            // Sideways Acceleration
+            if (controls.onPress(Keys.Right, Buttons.DPadRight))
+                x_accel += speed;
+            else if (controls.onRelease(Keys.Right, Buttons.DPadRight))
+                x_accel -= speed;
+            if (controls.onPress(Keys.Left, Buttons.DPadLeft))
+                x_accel -= speed;
+            else if (controls.onRelease(Keys.Left, Buttons.DPadLeft))
+                x_accel += speed;
+
+            // Sideways Movement
+            x_vel = x_vel * (1 - friction)
+                + (frozen() ? 0 : x_accel * .10);
+            spriteX += Convert.ToInt32(x_vel);
+
+            // Determine direction
+            if (x_vel > 0.1)
+                left = false;
+            else if (x_vel < -0.1)
+                left = true;
+
+            // Gravity
+            if (spriteY < physics.ground)
+            {
+                spriteY += y_vel;
+                y_vel += physics.gravity;
+            }
+            else
+            {
+                spriteY = physics.ground;
+                y_vel = 0;
+                grounded = true;
+            }
+        }
+
+        private void Shoot(Controls controls, Physics physics, ContentManager content)
+        {
             // New shots
             if (controls.onPress(Keys.D, Buttons.RightShoulder))
             {
@@ -95,17 +173,6 @@ namespace Puddle
             else if (controls.onRelease(Keys.D, Buttons.RightShoulder))
             {
                 shooting = false;
-            }
-
-            if (controls.onPress(Keys.S, Buttons.A) && grounded)
-            {
-                jump_point = physics.count;
-            }
-            else if (controls.onRelease(Keys.S, Buttons.A))
-            {
-                // Cut jump short
-                if (y_vel < 0)
-                    y_vel /= 2;
             }
 
             // Deal with shot creation and delay
@@ -129,156 +196,92 @@ namespace Puddle
                     s.LoadContent(content);
                     physics.shots.Add(s);
 
-                    // Upwards
+                    // Slight upward boost
                     spriteY -= 1;
                     y_vel = -8;
                 }
             }
+        }
 
-            // Jump logic
+        private void Jump(Controls controls, Physics physics)
+        {
+            // Jump on press
             if (controls.isPressed(Keys.S, Buttons.A) && !frozen() && grounded)
             {
                 spriteY -= 1;
                 y_vel = -15;
                 grounded = false;
+                jump_point = physics.count;
             }
 
-            Animate(controls, physics);
-            
-            CheckCollisions(physics);
-        }
-
-
-        public void CheckCollisions(Physics physics)
-        {
-            // Check enemy collisions
-            if (!puddled)
+            // Cut jump short on release
+            else if (controls.onRelease(Keys.S, Buttons.A) && y_vel < 0)
             {
-                foreach (Enemy e in physics.enemies)
-                {
-                    if (Math.Sqrt(Math.Pow(spriteX - e.spriteX, 2) +
-                        Math.Pow(spriteY - e.spriteY, 2)) < 32)
-                    {
-                        spriteX = 400;
-                        spriteY = -32;
-                        y_vel = 0;
-                        grounded = false;
-                    }
-                }
-            }
-
-            // TODO: Check other collisions
-        }
-
-        private void Move(Controls controls, Physics physics)
-        {
-            // Sideways Acceleration
-            if (controls.onPress(Keys.Right, Buttons.DPadRight))
-                x_accel += speed;
-            else if (controls.onRelease(Keys.Right, Buttons.DPadRight))
-                x_accel -= speed;
-            if (controls.onPress(Keys.Left, Buttons.DPadLeft))
-                x_accel -= speed;
-            else if (controls.onRelease(Keys.Left, Buttons.DPadLeft))
-                x_accel += speed;
-
-            // Sideways Movement
-            x_vel = x_vel * (1 - friction)
-                + (frozen() ? 0 : x_accel * .10);
-            spriteX += Convert.ToInt32(x_vel);
-
-            // Gravity
-            if (spriteY < physics.ground)
-            {
-                spriteY += y_vel;
-                y_vel += physics.gravity;
-            }
-            else
-            {
-                spriteY = physics.ground;
-                y_vel = 0;
-                grounded = true;
+                y_vel /= 2;
             }
         }
 
         private void Animate(Controls controls, Physics physics)
         {
-            // Determine direction
-            if (x_vel > 0.1)
-                left = false;
-            else if (x_vel < -0.1)
-                left = true;
-
-            // Determine movement
+            // Determine type of movement
             if (!frozen())
             {
                 // Jumping
                 if (!grounded)
                 {
+                    // Initialize sprite
                     if (image != images["jump"])
                     {
                         image = images["jump"];
                         frameIndex = 0;
                     }
+                    // Animate
+                    else if (frameIndex < 2 * 32)
+                        frameIndex += 32;
                 }
-                // Not Moving
+                // Grounded, not Moving
                 else if (Math.Abs(x_vel) < .5)
                 {
+                    // Initialize sprite. No animation.
                     if (image != images["stand"])
                     {
                         image = images["stand"];
                         frameIndex = 0;
                     }
                 }
-
-                // Yes moving
-                else if (image != images["walk"])
+                // Grounded, yes moving
+                else
                 {
-                    image = images["walk"];
-                    frameIndex = 0;
+                    // Initialize sprite
+                    if (image != images["walk"])
+                        image = images["walk"];
+                    // Animate
+                    frameIndex = (physics.count / 8 % 4) * 32;
                 }
             }
 
-
-            // Puddle sprite logic
+            // Puddle, if puddling
             if (puddled)
             {
+                // Initialize sprite
                 if (image != images["puddle"])
                 {
                     image = images["puddle"];
                     frameIndex = 0;
                 }
-                
-                if (controls.isPressed(Keys.Down, Buttons.DPadDown))
+                // Animate downward if puddling
+                else if (controls.isPressed(Keys.Down, Buttons.DPadDown))
                 {
                     if (frameIndex < 5 * 32)
                         frameIndex += 32;
                 }
+                // Animate upward if unpuddling
                 else
                 {
                     frameIndex -= 32;
                     if (frameIndex <= 0)
                         puddled = false;
                 }
-            }
-
-            // Walking sprite frames
-            else if (image == images["walk"])
-            {
-                frameIndex = (physics.count / 8 % 4) * 32;
-            }
-
-            // Jumping sprite frames
-            else if (image == images["jump"])
-            {
-                if (frameIndex < 2 * 32)
-                    frameIndex += 32;
-            }
-
-            // Standing sprite frames
-            else
-            {
-                frameIndex = 0;
             }
         }
 
